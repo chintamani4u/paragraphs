fd6214bdc853242d3fbc89f15ac46b995a21b2e7
Gergely Pap <gpap@brainsum.com>
2016-08-04 13:02:00 +0200

    TIET-3: mobile first design almost done

---
+++ themes/tieto_admin/js/scripts.js
@@ -0,0 +1,14 @@
+/**
+ * @file
+ * Scripts for theme.
+ */
+(function ($) {
+  'use strict';
+
+  $('.r-tabs-container').responsiveTabs({
+      startCollapsed: false,
+      animation: 'slide',
+      duration: 200
+  });
+
+})(jQuery);
