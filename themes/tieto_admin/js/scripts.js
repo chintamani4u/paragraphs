ce621e88caaf39699a862b7ecbba03830660d91c
Gergely Pap <gpap@brainsum.com>
2016-08-04 15:37:41 +0200

    TIET-3: updates

--- themes/tieto_admin/js/scripts.js
+++ themes/tieto_admin/js/scripts.js
@@ -3,12 +3,31 @@
  * Scripts for theme.
  */
 (function ($) {
-  'use strict';
+	'use strict';
 
-  $('.r-tabs-container').responsiveTabs({
-      startCollapsed: false,
-      animation: 'slide',
-      duration: 200
-  });
+	$('.r-tabs-container').responsiveTabs({
+	  startCollapsed: false,
+	  animation: 'slide',
+	  duration: 200
+	});
+
+	var hamburger = $('#hamburger');
+	var header = $('.page-header');
+
+	hamburger.on('click', function () {
+		$(this).toggleClass('is-active');
+	});
+
+	$(document).ready(function() {
+        header.scrollspy({
+            min: 500,
+            onEnter: function(element, position) {
+                header.addClass('fixed');
+            },
+            onLeave: function(element, position) {
+                header.removeClass('fixed');
+            }
+        });
+    });
 
 })(jQuery);
