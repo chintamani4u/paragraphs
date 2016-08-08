adf2dede11d366116fddcdc54d43b7685ff79e46
Gergely Pap <gpap@brainsum.com>
2016-08-08 12:49:06 +0200

    TIET-3: changed loading of onepage libraries

--- themes/tieto_admin/js/scripts.js
+++ themes/tieto_admin/js/scripts.js
@@ -5,30 +5,9 @@
 (function ($) {
 	'use strict';
 
-	$('.r-tabs-container').responsiveTabs({
-	  startCollapsed: false,
-	  animation: 'slide',
-	  duration: 200
-	});
-
 	var hamburger = $('#hamburger');
-	var header = $('.page-header');
-
 	hamburger.on('click', function () {
 		$(this).toggleClass('is-active');
 	});
-
-	$(document).ready(function() {
-        header.scrollspy({
-            min: 490,
-            max: 10000,
-            onEnter: function(element, position) {
-                header.addClass('fixed');
-            },
-            onLeave: function(element, position) {
-                header.removeClass('fixed');
-            }
-        });
-    });
 
 })(jQuery);
