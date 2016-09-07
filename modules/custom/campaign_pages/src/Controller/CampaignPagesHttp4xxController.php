12c463ce171e5ee9ca05b2c3ecae637df9f2ee8c
szato <tszanyi@brainsum.com>
2016-09-07 13:55:51 +0200

    TCS-27 Custom twig for 404, 403 pages, front page chnaged to 404

---
+++ modules/custom/campaign_pages/src/Controller/CampaignPagesHttp4xxController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Drupal\campaign_pages\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+ * Controller for default HTTP 4xx responses.
+ */
+class CampaignPagesHttp4xxController extends ControllerBase {
+
+  /**
+   * The default 403 content.
+   *
+   * @return array
+   *   A render array containing the message to display for 404 pages.
+   */
+  public function on403() {
+    return [
+      '#theme' => 'campaign_pages_403',
+      '#error_code' => '403',
+    ];
+  }
+
+  /**
+   * The default 404 content.
+   *
+   * @return array
+   *   A render array containing the message to display for 404 pages.
+   */
+  public function on404() {
+    return [
+      '#theme' => 'campaign_pages_404',
+      '#error_code' => '404',
+    ];
+  }
+
+}
