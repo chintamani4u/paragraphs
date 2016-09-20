645716dafcbc42dfbb07ea25de22a7e51955d67c
szato <tszanyi@brainsum.com>
2016-09-20 13:14:59 +0200

    TCS-30 custom translation switcher block - alfa

---
+++ modules/custom/campaign_pages/src/Plugin/Block/TranslationSwitcher.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Drupal\campaign_pages\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a 'Translation switcher' block.
+ *
+ * @Block(
+ *   id = "campaign_pages_translation_switcher",
+ *   admin_label = @Translation("Transaltion switcher"),
+ * )
+ */
+class TranslationSwitcher extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $current_uri = \Drupal::request()->getRequestUri();
+
+    $en = <<<EOL
+<ul class="links">
+	<li class="en is-active" data-drupal-link-system-path="/kth-breakfast-seminars" hreflang="en"><a class="language-link" href="/kth-breakfast-seminars" hreflang="en">English</a></li>
+	<li class="sv" data-drupal-link-system-path="/sv/kth-frukostseminarium" hreflang="sv"><a class="language-link" href="/sv/kth-frukostseminarium" hreflang="sv">Swedish</a></li>
+</ul>
+EOL;
+    $sv = <<<EOL
+<ul class="links">
+	<li class="en" data-drupal-link-system-path="/kth-breakfast-seminars" hreflang="en"><a class="language-link" href="/kth-breakfast-seminars" hreflang="en">English</a></li>
+	<li class="sv is-active" data-drupal-link-system-path="/sv/kth-frukostseminarium" hreflang="sv"><a class="language-link" href="/sv/kth-frukostseminarium" hreflang="sv">Swedish</a></li>
+</ul>
+EOL;
+
+    switch ($current_uri) {
+      case '/kth-breakfast-seminars':
+        $output = $en;
+        break;
+
+      case '/sv/kth-frukostseminarium':
+        $output = $sv;
+        break;
+    }
+
+    return array(
+      '#markup' => $output,
+      '#attributes' => [
+        'class' => ['block-language'],
+      ],
+    );
+  }
+
+}
