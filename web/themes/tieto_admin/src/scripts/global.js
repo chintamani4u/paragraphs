075e2d4f78c1b17056f3916305f626dd3b388f19
Daniel <d@niel.hu>
2016-11-21 23:24:18 +0100

    Increased header height

--- web/themes/tieto_admin/src/scripts/global.js
+++ web/themes/tieto_admin/src/scripts/global.js
@@ -11,4 +11,11 @@
 		hamburger.toggleClass('is-active')
 	})
 
+	// Updates parallax header height with px value instead of vh, so it won't be jumping on Android
+	// @todo - remove this from here
+	const $header = $('.paragraph--type--header.paragraph--view-mode--default');
+	if ($header.length) {
+		$header.height($header.height());
+	}
+
 })(jQuery)
